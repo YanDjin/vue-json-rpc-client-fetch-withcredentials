32db84dcf8f9e77ba5580f481f48668b03ada3e1
Yanis Bouzidi <yanis.bouzidi@kwanko.com>
2020-04-07 15:46:05 +0200

    master : changed the jsonRPC package to handle function for conditions

--- index.js
+++ index.js
@@ -3,9 +3,9 @@
 import JsonRpcVueClient from "json-rpc-client-fetch-withcredentials";
 
 export default {
-  install: function(Vue, url = url, credentials = "include", newHeaders = null, debug = false, callbacksByStatusCode = {}) {
-    let client = new JsonRpcVueClient(url, credentials, newHeaders, debug, callbacksByStatusCode);
+  install: function (Vue, url = url, credentials = "include", newHeaders = null, debug = false, callbacksOnResponse = []) {
+    let client = new JsonRpcVueClient(url, credentials, newHeaders, debug, callbacksOnResponse);
 
     Object.defineProperty(Vue.prototype, "$JsonRpcVueClient", { value: client });
-  }
+  },
 };
